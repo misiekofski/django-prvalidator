2109afd5d5eac1618fd8a3aeb927c451bedfa2f0
Michal Dobrzycki <dobrzycki@gmail.com>
2019-05-04 16:42:43 +0200

    Added customization of Admin module and refactored models

--- validator/admin.py
+++ validator/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import PR, Repository
 
 # Register your models here.
-admin.site.register(PR)
-admin.site.register(Repository)
+@admin.register(PR)
+class PRAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'name', 'status', 'repo')
+    list_editable = ('owner', 'status')
+
+@admin.register(Repository)
+class RepositoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'name', 'status')
+    list_editable = ('owner', 'status')
--- validator/models.py
+++ validator/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+REPO_STATUSES = (
+    ('A', 'Active'),
+    ('D', 'Deleted'),
+    ('N', 'Not active'),
+    ('P', 'Production ready'),
+    ('T', 'Test')
+)
+
+PR_STATUSES = (
+    ('A', 'Active'),
+    ('D', 'Deleted'),
+    ('N', 'Not active'),
+    ('M', 'Merged to master'),
+    ('T', 'Test')
+)
 
 # Create your models here.
 class Repository(models.Model):
@@ -8,24 +23,13 @@ class Repository(models.Model):
     name = models.CharField(max_length=100, default="Unnamed Repository")
     description = models.CharField(max_length=1000, null=True)
     created = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=1, default='N')
-
-    def __str__(self):
-        return "Repository {0}, owned by: {1}".format(
-            self.name, self.owner
-        )
-
+    status = models.CharField(max_length=1, default='N', choices=REPO_STATUSES)
 
 class PR(models.Model):
     owner = models.ForeignKey(User, related_name="pr_creator", on_delete=models.PROTECT)
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=100, default="Unnamed PR")
     description = models.CharField(max_length=1000, null=True)
-    status = models.CharField(max_length=1, default='N')
+    status = models.CharField(max_length=1, default='N', choices=PR_STATUSES)
     merged = models.BooleanField()
     repo = models.ForeignKey(Repository, related_name="repository", on_delete=models.CASCADE)
-
-    def __str__(self):
-        return "PR {0}, created by: {1}".format(
-            self.name, self.owner
-        )
