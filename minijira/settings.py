1d944a61b22e7aac93c2ef2dd908672ff62357d1
Michal Dobrzycki <dobrzycki@gmail.com>
2019-06-14 12:45:50 +0200

    Changed settings

--- minijira/settings.py
+++ minijira/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'projects.urls'
+ROOT_URLCONF = 'minijira.urls'
 
 TEMPLATES = [
     {
--- minijira/urls.py
+++ minijira/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('projects.urls')),
     path('admin/', admin.site.urls),
-
 ]
